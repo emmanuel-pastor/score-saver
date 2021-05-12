1f3e843ea4bbfd05c2772712241136564a3370c3
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-05-12 13:01:39 +0200

    Add footer styling

--- app/_config/template.php
+++ app/_config/template.php
@@ -16,9 +16,16 @@
     }
     ?>
 </nav>
-<br/>
+
 <div id="corps"><?php echo $corps; ?></div>
-<footer>MOOC AppDyn</footer>
+
+<footer>
+    <p>Développé par <u>Emmanuel Pastor</u></p>
+    <a href="https://www.github.com/emmanuel-pastor" target={'_blank'} rel={'noopener noreferrer'}><img src=<?php echo BASE_PATH . "/img/logo-github.svg" ?> alt="Logo de Github"></a>
+    <a href="https://www.linkedin.com/in/emmanuel-pastor-9a9a82201" target={'_blank'} rel={'noopener noreferrer'}><img src=<?php echo BASE_PATH . "/img/logo-linkedin.svg" ?> alt="Logo de LinkedIn"></a>
+    <a href="https://www.simple-smart-apps.com" target={'_blank'} rel={'noopener noreferrer'}><img src=<?php echo BASE_PATH . "/img/ic-internet.svg" ?> alt="Icône Internet"></a>
+    <img class="logo-insa" src=<?php echo BASE_PATH . "/img/logo-insa.png" ?> alt="Logo de l'INSA Toulouse">
+</footer>
 
 </body>
 </html>
