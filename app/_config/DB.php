29070416c0ab280a9351007eea2a69d70d636ac2
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-05-15 12:34:00 +0200

    Create tables if not created (in case of 1st use of the server)

--- app/_config/DB.php
+++ app/_config/DB.php
@@ -1,9 +1,45 @@
 <?php
 function openConnection(): mysqli
 {
-    return new mysqli(HOST_NAME, DB_USER, DB_PASSWORD, DB_NAME);
+    createUserTableIfNotExists();
+    createScoreTableIfNotExists();
+
+    return openBaseConnection();
 }
 
-function closeConnection($connection) {
+function createUserTableIfNotExists()
+{
+    $db = openBaseConnection();
+    $userTableCreationQuery = "CREATE TABLE IF NOT EXISTS `user` ( 
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(256) NOT NULL,
+    `password` varchar(256) NOT NULL,
+    `is_valid` tinyint(1) NOT NULL DEFAULT '0',
+    `validation_key` varchar(256) NOT NULL,
+    PRIMARY KEY (`id`) )
+    ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+    $db->query($userTableCreationQuery);
+    closeConnection($db);
+}
+
+function createScoreTableIfNotExists() {
+    $db = openBaseConnection();
+    $scoreTablesCreationQUery = "CREATE TABLE IF NOT EXISTS `score` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `value` int(5) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+    $db->query($scoreTablesCreationQUery);
+    closeConnection($db);
+}
+
+function openBaseConnection() {
+    return mysqli_connect(HOST_NAME, DB_USER, DB_PASSWORD, DB_NAME);
+}
+
+function closeConnection($connection)
+{
     mysqli_close($connection);
 }
