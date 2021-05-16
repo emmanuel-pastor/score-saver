d9f023c6750e48ad495d533c9dc1e85410f35ca5
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-05-16 20:49:35 +0200

    Fix media queries not being respected on phones

--- app/_config/template.php
+++ app/_config/template.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href=<?php echo BASE_PATH . "css/template.css" ?> rel="stylesheet"/>
     <link href=<?php $stylesheet = $stylesheet ?? ''; echo BASE_PATH . "css/" . $stylesheet ?> rel="stylesheet"/>
     <script  src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
